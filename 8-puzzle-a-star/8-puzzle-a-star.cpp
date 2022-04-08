a5a2a0c7c314ea3dfcf2444ff82e3c533f7a5e81
Suklav <suklavghosh2016@gmail.com>
2022-04-08 13:27:26 +0530

    test11

--- 8-puzzle-a-star/8-puzzle-a-star.cpp
+++ 8-puzzle-a-star/8-puzzle-a-star.cpp
@@ -2,7 +2,7 @@
 //-------------------------------------------------------------------------
 
 //8 puzzle problem using A star algorithm.
-//test2
+//test21
 #include<iostream>
 #include<vector>
 #include<list>
